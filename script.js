acf2e411dda7fa7884b78392b13d365f4fdf4fcd
fernandoarruda <fernandoarruda1384@gmail.com>
2022-01-27 23:06:33 -0300

    update arrays

---
+++ script.js
@@ -0,0 +1,14 @@
+//como declarar array
+
+let array = ["string", 1, true, ['array1'], ['array2'], ['array3']];
+console.log(array);
+
+let array1 = [ 1, 13, "x"];
+console.log(array1);
+
+let array2 = ["Spider-man", "Vulture", "Mary Jane"];
+console.log(array2[2]);
+
+let array3 = ["Geralt", "Yennefer", "ciri"];
+console.log(array3[2]);
+
